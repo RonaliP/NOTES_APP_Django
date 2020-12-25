9b79da88dae2cbc1518ce113032f61325c880662
Ronali <ronalipanigrahy88@gmail.com>
2020-12-25 21:58:57 +0530

    For sending token to user's email Email_backend,host,host user,host password added

--- TODO_LIST/settings.py
+++ TODO_LIST/settings.py
@@ -128,3 +128,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS=True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='ronalipanigrahy88@gmail.com'
+EMAIL_HOST_PASSWORD='123@we$*'
+
