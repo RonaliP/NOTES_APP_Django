1785ab76a48a838d2229331cdf958fcfdc6861a8
Ronali <ronalipanigrahy88@gmail.com>
2020-12-25 22:06:15 +0530

    For sending access token to user's email ,utils.py added and in views.py to access user's email code added

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from rest_framework import generics,status
 from .serializers import RegisterSerializer
 from rest_framework.response import Response
-
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+from .utils import Util
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
 # Create your views here.
 
 class RegisterView(generics.GenericAPIView):
@@ -11,11 +15,32 @@ class RegisterView(generics.GenericAPIView):
 
 
     def post(self,request):
+
         user=request.data
         serializer=self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
         user_data=serializer.data
 
+       #added below to lines to access the email of user
+
+        user=User.objects.get(email=user_data['email'])
+        token=RefreshToken.for_user(user).access_token
+
+        current_site=get_current_site(request).domain
+        relativeLink=reverse('email-verify')
+        absurl = 'http://' + current_site + relativeLink + "?token=" + str(token)
+        email_body='Hey'+ user.username +'use link below to verify your email and activate your account\n'+ absurl
+        data={'email_body':email_body,
+              'to_email':user.email,
+              'email_subject':'verify email'}
+        Util.send_email(data)
+
+
         return Response(user_data,status=status.HTTP_201_CREATED)
+
+
+#The below code will verify the email and activate the token sent to user's emailid
+class VerifyEmail(generics.GenericAPIView):
+    def get(self):
+        pass
