17e2df6088b68dafeed5b1b2f0a17dee26fefdf5
Ronali <ronalipanigrahy88@gmail.com>
2021-01-17 19:30:46 +0530

    collaborator view added

--- Notes/views.py
+++ Notes/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from Notes.serializers import NotesSerializer, LabelsSerializer, \
-    ArchiveNotesSerializer, TrashSerializer, AddLabelsToNoteSerializer
+    ArchiveNotesSerializer, TrashSerializer, AddLabelsToNoteSerializer,Notescollaborator
 from Notes.permissions import IsOwner
 from Notes.models import Notes, Labels
+from django.db.models import Q
 from rest_framework import generics, permissions, status
 # FOR CACHING
 from django.conf import settings
@@ -12,6 +13,7 @@ from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.core.cache import cache
 from django.views.decorators.cache import cache_page
 import logging
+import json
 
 
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
@@ -19,10 +21,11 @@ logger = logging.getLogger('django')
 
 # Create your views here.
 
-class CreateNotes(generics.CreateAPIView):
+class CreateNotes(generics.ListCreateAPIView):
     """This Api will create notes for the current user """
     serializer_class = NotesSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+    queryset = Notes.objects.all()
 
     def perform_create(self, serializer):
         owner = self.request.user
@@ -33,12 +36,19 @@ class CreateNotes(generics.CreateAPIView):
             logger.info("DATA STORED IN CACHE")
             return Response({'success': 'New note is created!!'}, status=status.HTTP_201_CREATED)
 
+    def get_queryset(self):
+        """ Get notes list owned by current logged in user """
+        owner = self.request.user
+        return self.queryset.filter(Q(owner=owner) | Q(collaborator=owner), Q(isArchive=False, isDelete=False))
+
 
 class DisplayNotes(generics.ListAPIView):
     """This Api will list out all the notes of current user from database """
     serializer_class = NotesSerializer
     queryset = Notes.objects.all()
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+
     """
     def get_queryset(self, notes=None):
         if cache.get(notes):
@@ -49,15 +59,40 @@ class DisplayNotes(generics.ListAPIView):
             cache.set(notes, data)
             print("data saved in cache")
             return cache.get(notes)
-
+            
     """
-class Notesearch(generics.ListAPIView):
-    """This is a search note api which will search for the existing notes"""
-    queryset = Notes.objects.all()
-    serializer_class = NotesSerializer
-    filter_backends = [SearchFilter]
-    search_fields = ['title']
 
+
+class Notesearch(generics.GenericAPIView):
+    """This is a search note api which will search for the existing notes"""
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+    serializer_class = NotesSerializer
+
+    def get_queryset(self, queryset=None):
+        notes = []
+        owner = self.request.user
+        if queryset:
+            searchlist = queryset.split(' ')
+            if cache.get(queryset):
+                notes = cache.get(queryset)
+                logger.info("data is coming from cache")
+            else:
+                for query in searchlist:
+                    notes = Notes.objects.filter(Q(title__icontains=query) | Q(content__icontains=query))
+                    if notes:
+                        cache.set(queryset, notes)
+        else:
+            notes = Notes.objects.all()
+        return notes
+
+    def get(self, request):
+        queryset = request.GET.get('search')
+        if queryset:
+            note = self.get_queryset(queryset)
+        else:
+            note = self.get_queryset()
+        serializer = NotesSerializer(note, many=True)
+        return Response({'response_data': serializer.data}, status=status.HTTP_200_OK)
 
 class NoteDetails(generics.RetrieveUpdateDestroyAPIView):
     """API to retrieve, update, and delete note by id """
@@ -210,3 +245,24 @@ class ListNotesInLabel(generics.ListAPIView):
 
     def get_queryset(self):
         return self.queryset.filter(owner=self.request.user, label=self.kwargs[self.lookup_field])
+
+class AddCollaboratorForNotes(generics.RetrieveUpdateAPIView):
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+    serializer_class = Notescollaborator
+    queryset = Notes.objects.all()
+    lookup_field = "id"
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        return Response({'success':'Your new Collaborator is added'})
+
+    def get_queryset(self):
+        return self.queryset.filter(owner=self.request.user)
+
+
+
+
+
+
+
+
