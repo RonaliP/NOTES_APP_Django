c081e718b03bbc702ae7a56532283426ab9efcc5
Ronali <ronalipanigrahy88@gmail.com>
2021-01-17 19:23:58 +0530

    collaboratorserializer added

--- Notes/serializers.py
+++ Notes/serializers.py
@@ -1,12 +1,38 @@
 from rest_framework import serializers
 from Notes.models import Notes, Labels
+from authentication.models import User
+from rest_framework.renderers import JSONRenderer
 
 
 class NotesSerializer(serializers.ModelSerializer):
+    collaborator = serializers.StringRelatedField()
+
     class Meta:
         model = Notes
-        fields = ['title', 'content', 'isArchive', 'isDelete', ]
-        extra_kwargs = {'isDelete': {'read_only': True}, 'isArchive': {'read_only': True}, }
+        fields = ['title', 'content', 'isArchive', 'isDelete','owner_id', 'collaborator']
+        extra_kwargs = {'isDelete': {'read_only': True},
+                        'isArchive': {'read_only': True}, 'collaborator': {'read_only': True},
+                        'owner_id': {'read_only': True}
+                        }
+
+        def validate(self, data):
+            title = data.get('title','')
+            content = data.get('content','')
+            return data
+
+
+
+
+
+class Notescollaborator(serializers.ModelSerializer):
+    collaborator = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    class Meta:
+        model = Notes
+        fields = ['title', 'content','owner_id', 'collaborator']
+        extra_kwargs = {'owner': {'read_only': True},
+                        'title': {'read_only': True},
+                        'content': {'read_only': True}}
 
 
 class LabelsSerializer(serializers.ModelSerializer):
