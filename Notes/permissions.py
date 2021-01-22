9851c934d1c883efcc0e4d36e96fca5ad78326ae
Ronali <ronalipanigrahy88@gmail.com>
2021-01-22 18:29:14 +0530

    To add multiple collaborator permission added

--- Notes/permissions.py
+++ Notes/permissions.py
@@ -8,3 +8,16 @@ class IsOwner(permissions.BasePermission):
             return True
         else:
             return False
+
+class IsCollaborator(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        collaborators = []
+        collaborators = obj.collaborator.all()
+        if collaborators:
+            for collaborator in collaborators:
+                if collaborator == request.user:
+                    return True
+        elif obj.owner == request.user:
+            return True
+        else:
+            return False
